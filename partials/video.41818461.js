165eba71b8da2c566dadf90ae6fda31520ee3676
cristiangarzon22 <86244196+cristiangarzon22@users.noreply.github.com>
2023-06-04 04:24:37 +0000

    Deploying to gh-pages from @ cristiangarzon22/goit-js-08@b1c135e0eeeeec161a076c90e9ecd27eb56f9586 🚀

--- partials/video.41818461.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("timeupdate",(function(e){setInterval(console.log("played the video!",e.seconds),4e3)})),o.getVideoTitle().then((function(e){console.log("title:",e)}));o.on("play",(function(e){}))}();
-//# sourceMappingURL=video.41818461.js.map
